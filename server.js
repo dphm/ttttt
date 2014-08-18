54a74109163a2f42f24da9790ef352b0abc46e22
quelledanielle <quelledanielle@gmail.com>
2014-08-18 17:55:18 -0400

    Add menu and write game string to socket

--- server.js
+++ server.js
@@ -3,14 +3,23 @@ var net = require('net');
 var HOST = '127.0.0.1';
 var PORT = 8484;
 
+var ttt = require('./game');
+var games = [];
+
 var server = net.createServer(function(socket) {
   console.log('Client connected: ' + socket.remoteAddress + ':' + socket.remotePort);
 
-  process.stdin.resume();
-  process.stdin.pipe(socket);
+  socket.write('MENU\n' +
+               '====\n' +
+               'CREATE: create a new tic tac toe game\n');
   
   socket.on('data', function(data) {
     console.log(data.toString());
+    if (data.toString().indexOf('CREATE') !== -1) {
+      var game = new ttt.Game();
+      games.push(game);
+      socket.write(game.toString());
+    }
   });
 
   socket.on('end', function() {
